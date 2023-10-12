56469cddb8840112c8fb608fe62275ef8f26009c
eldivategar <tegareldiva@students.amikom.ac.id>
2023-10-12 14:32:18 +0700

    update

--- a_core/settings.py
+++ a_core/settings.py
@@ -15,9 +15,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-import mimetypes
-mimetypes.add_type("text/css", ".css", True)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
