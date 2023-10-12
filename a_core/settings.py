a084d0d8b6cab2febdfef7a1449dfdd872b58526
eldivategar <tegareldiva@students.amikom.ac.id>
2023-10-12 14:04:02 +0700

    update static files

--- a_core/settings.py
+++ a_core/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2g68zz*#t#9c_hf3b+n1uijxzr$v$4+4$jq!iwj#pd@74uwc&k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Add these new lines
 STATICFILES_DIRS = (
