6b32345d8976ce7db8b57b703b5141cc636d80c3
eldivategar <tegareldiva@students.amikom.ac.id>
2023-10-13 14:13:00 +0700

    a

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,19 @@
-// const csrftoken = getCookie("csrftoken");
-var csrftoken = '{{ csrf_token }}';
-
 function getCookie(name) {
-  var cookieValue = null;
-  if (document.cookie && document.cookie !== "") {
-    var cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++) {
-      var cookie = cookies[i].trim();
-      if (cookie.substring(0, name.length + 1) === name + "=") {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
       }
-    }
   }
   return cookieValue;
 }
+const csrftoken = getCookie('csrftoken');
 
 const upd = (url) => {
   Swal.fire({
